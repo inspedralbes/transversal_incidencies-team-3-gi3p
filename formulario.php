d9cace8675e6810d189252119fcf2f6553fc8d5e
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-03-15 13:18:07 +0100

    Ya no esta hardcode

--- formulario.php
+++ formulario.php
@@ -1,14 +1,28 @@
-<?php include_once "encabezado.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once "encabezado.php"; ?>
+</head>
+<body>
 <div class="row">
     <div class="col-12">
         <h1>Registrar incidencia</h1>
         <form action="registrar.php" method="POST">
             <div class="form-group">
                 <label for="clase">Departament</label>
-                <select name="clase" id="clase" >
-                    <option value="CIE">Ciencias</option>
-                    <option value="MAT">Matematicas</option>
-                    <option value="CAT">Catalan</option>
+                <select name="clase" id="clase">
+                    <?php
+                     include_once "conexion.php";
+
+                    $sentencia = $mysqli->query("SELECT idDep, nom FROM DEPARTAMENT");
+                    $departaments = $sentencia->fetch_all(MYSQLI_ASSOC);
+                
+                        foreach ($departaments as $departament) { ?>
+
+                        <option value="<?php echo $departament["idDep"];?>"> <?php echo $departament["nom"];?> </option> <?php
+
+                    } ?>
+                    
                 </select>
             </div>
             <div class="form-group">
@@ -24,4 +38,5 @@
         </form>
     </div>
 </div>
-<?php include_once "pie.php"; ?>
+</body>
+</html>
