6a5cb1f76330a012f49bf44b592c8e41070dabc2
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-03-20 14:03:18 +0100

    Update llistaincidencies.php

--- llistaincidencies.php
+++ llistaincidencies.php
@@ -19,7 +19,7 @@
         </div>
         <?php
         $mysqli = include_once "conexion.php";
-        $resultado = $mysqli->query("SELECT idInc, descripcio, prioritat, DEPARTAMENT.nom as departament , CAST(INCIDENCIA.data AS date) AS data, USUARIO.nombre as tecnic FROM INCIDENCIA JOIN DEPARTAMENT ON DEPARTAMENT.idDep = INCIDENCIA.departament LEFT JOIN USUARIO ON USUARIO.id_User = INCIDENCIA.tecnic WHERE INCIDENCIA.dataFi IS NULL ORDER BY idInc");
+        $resultado = $mysqli->query("SELECT idInc, descripcio, prioritat, DEPARTAMENT.nom as departament , CAST(INCIDENCIA.data AS date) AS data, CONCAT(USUARIO.nombre,' ',USUARIO.pApellido,' ',USUARIO.sApellido) as tecnic FROM INCIDENCIA JOIN DEPARTAMENT ON DEPARTAMENT.idDep = INCIDENCIA.departament LEFT JOIN USUARIO ON USUARIO.id_User = INCIDENCIA.tecnic WHERE INCIDENCIA.dataFi IS NULL ORDER BY idInc");
         $incidencies = $resultado->fetch_all(MYSQLI_ASSOC);
         
         foreach ($incidencies as $incidencia) { ?>
@@ -29,13 +29,13 @@
             <p class="campIncidenciaId"><?php echo $incidencia["idInc"] ?></p>
             <p class="campIncidenciaDep"><?php echo $incidencia["departament"] ?></p>
             <p class="campIncidenciaDescripcio"><?php echo $incidencia["descripcio"] ?></p>
-            <p class="campIncidenciaPrio"><?php
-            if(empty($incidencia["prioritat"])){
+            <p class="campIncidenciaPrio"> <?php 
+            
+            if (empty($incidencia["prioritat"])) {
                 echo "Sense assignar";
-            } else{
-                echo $incidencia["prioritat"] ;
-            } 
-            ?></p>
+            } else {
+                echo $incidencia["prioritat"];
+            } ?> </p>
             <p class="campIncidenciaData"><?php echo $incidencia["data"] ?></p>
             <p class="campIncidenciaTecnic"><?php 
             
@@ -44,8 +44,11 @@
             } else {
                 echo $incidencia["tecnic"];
             } 
+
             ?> </p>
             <a class="botoEditar" href="editar.php?idInc=<?php echo $incidencia["idInc"] ?>">Editar</a>
+
+
         </div>
 
         <?php } ?>
