f36fbf5fd686b703ccabf14b7a485f7590990327
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-03-20 10:25:45 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$usuario = "a22lorcrinor_bd";
-$contrasenia = "InsPedralbes2022";
-$base_de_datos = "a22lorcrinor_incidencies";
+$usuario = "a22rauespgom_G3";
+$contrasenia = "RaulLorisGalaEric1";
+$base_de_datos = "a22rauespgom_G3_Incidencies";
 $mysqli = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
 if ($mysqli->connect_errno) {
     echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
