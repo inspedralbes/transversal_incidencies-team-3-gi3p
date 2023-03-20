b2a20a8a3d154becb4983f7956a0775e1d64498c
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2023-03-20 14:09:30 +0100

    Update consultar.php

--- consultar.php
+++ consultar.php
@@ -9,10 +9,13 @@ $resultado = $sentencia->get_result();
 # Obtenemos solo una+ fila, que será el incidencia a editar
 $incidencia = $resultado->fetch_assoc();
 
+
 if (!$incidencia) {
     exit("No hay resultados para ese ID");
 }
 
+$resultado= $mysqli->query("SELECT descripcio,data,temps,idAct FROM ACTUACIO WHERE incidencia = $id AND visible = 1 ORDER BY idAct");
+$actuacions = $resultado->fetch_all(MYSQLI_ASSOC);
 ?>
 <section>
     <div class="con-desc"><h3>Descripció</h3>
@@ -34,5 +37,38 @@ if (!$incidencia) {
             } else {
                 echo "Solucionat el "; echo $incidencia['dataFi'];
             } ?> </p></div>
+    <h2>Actuacions</h2>
 
+
+
+<?php 
+if (!$actuacions) {
+
+    echo "No hi ha cap actuació realitzada";
+    }else{ ?>
+        <table class="table table-striped table-light">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Data</th>
+                <th scope="col">Descripcio</th>
+                <th scope="col">Temps</th>
+            </tr>
+        </thead>
+        <tbody>
+
+    <?php
+        foreach ($actuacions as $actuacio){ ?>
+            <tr>
+                <th scope="row"><?php echo $actuacio["idAct"] ?></th>
+                <td><?php echo $actuacio["data"] ?></td>
+                <td><?php echo $actuacio["descripcio"] ?></td>
+                <td><?php echo $actuacio["temps"] ?></td>
+            <tr>
+        <?php } 
+    } ?>
+
+
+</tbody>
+</table>
 </section>
