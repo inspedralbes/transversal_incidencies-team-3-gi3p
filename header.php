ab9a057a6c6ac18fa578fd7bdbf037cd045d72e9
a22rauespgom <123157094+a22rauespgom@users.noreply.github.com>
2023-03-30 08:13:11 +0200

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once "conexion.php"; 
-?>
-
-<header>
-<p>
-<?php echo $_SESSION["username"] ?></p>
-<a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-</header>
